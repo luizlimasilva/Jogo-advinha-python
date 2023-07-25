8aa2e7e614382b813f61d2d8c6c37e4f7b61da11
Luiz Silva <51297600+luizlimasilva@users.noreply.github.com>
2023-07-24 20:42:42 -0400

    Adicionando IF
    Adicionando o IF para indicar se o chute foi maior ou menor que o número secreto.

--- main.py
+++ main.py
@@ -8,8 +8,16 @@ chute = int(input("Digite o seu número: "))
 
 print("Você digitou ", chute)
 
-if(numero_secreto == chute):
+acertou = numero_secreto == chute
+maior   = chute > numero_secreto
+menor   = chute < numero_secreto
+
+if(acertou):
     print("Você acertou!!!")
 else:
-    print("Você errou!")
-    print("o número correto é: ", numero_secreto)
+    if(maior):
+        print("Você errou! O seu chute foi maior que o número secreto.")
+    elif(menor):
+        print("Você errou! O seu chute foi menor que o número secreto.")
+
+print("Fim de jogo!!!")
