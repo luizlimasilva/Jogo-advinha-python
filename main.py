db7e97cfeb2315cd1312499a34070258b1e9c8e2
Luiz Silva <51297600+luizlimasilva@users.noreply.github.com>
2023-07-24 21:07:19 -0400

    Uso do While
    acrescentando o uso do while e criando tentativas para acerto do número secreto.

--- main.py
+++ main.py
@@ -2,22 +2,30 @@ print("********************************")
 print("bem vindo ao jogo de advinhação!")
 print("********************************")
 
+
+
 numero_secreto = 42
+tentativas = 3
+rodada = 1
 
-chute = int(input("Digite o seu número: "))
+while(rodada <= tentativas):
+    print("Tentativa {} de {}".format(rodada,tentativas))
+    chute = int(input("Digite o seu número: "))
+    print("Você digitou ", chute)
 
-print("Você digitou ", chute)
+    acertou = numero_secreto == chute
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-acertou = numero_secreto == chute
-maior   = chute > numero_secreto
-menor   = chute < numero_secreto
+    if(acertou):
+        print("Você acertou!!!")
+        rodada = tentativas
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior que o número secreto.")
+        elif(menor):
+            print("Você errou! O seu chute foi menor que o número secreto.")
 
-if(acertou):
-    print("Você acertou!!!")
-else:
-    if(maior):
-        print("Você errou! O seu chute foi maior que o número secreto.")
-    elif(menor):
-        print("Você errou! O seu chute foi menor que o número secreto.")
+    rodada = rodada +1
 
 print("Fim de jogo!!!")
