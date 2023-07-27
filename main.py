4abb96992f948561c31c8a62a830aeaa9cbbfc3b
Luiz Silva <51297600+luizlimasilva@users.noreply.github.com>
2023-07-26 21:00:24 -0400

    Mudanças no jogo
    - Mudança do while pro for nas tentativas
    - Aviso indicativo "números entre 1 a 100"
    - Finalização do jogo ao acertar.

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ print("********************************")
 
 numero_secreto = 42
 tentativas = 3
-rodada = 1
 
-while(rodada <= tentativas):
+for rodada in range(1, tentativas +1):
     print("Tentativa {} de {}".format(rodada,tentativas))
     chute = int(input("Digite o seu número: "))
     print("Você digitou ", chute)
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!!!")
+        continue
 
     acertou = numero_secreto == chute
     maior = chute > numero_secreto
@@ -19,13 +22,12 @@ while(rodada <= tentativas):
 
     if(acertou):
         print("Você acertou!!!")
-        rodada = tentativas
+        break
     else:
         if(maior):
             print("Você errou! O seu chute foi maior que o número secreto.")
         elif(menor):
             print("Você errou! O seu chute foi menor que o número secreto.")
 
-    rodada = rodada +1
 
 print("Fim de jogo!!!")
